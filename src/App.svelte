c1a3fdaf05b838ce6641578bcb3d2b7b4557de3f
Rui Posse <ruiposse@gmail.com>
2022-01-29 22:33:55 +0000

    chore(udemy): Created first 'computed property' in lesson 012

--- src/App.svelte
+++ src/App.svelte
@@ -2,8 +2,14 @@
   let name = 'Max'
   let age = 30;
 
+  $: uppercaseName = name.toUpperCase();
+
   function incrementAge() {
     age += 1;
+  }
+
+  function changeName() {
+    name = 'Maximilian';
   }
 </script>
 
@@ -13,5 +19,6 @@
 	}
 </style>
 
-<h1>Hello {name}, my age is {age}!</h1>
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click="{incrementAge}">Change Age</button>
+<button on:click="{changeName}">Change Name</button>
